66ae5f4903fec181a65a5330e8b8942b4f0a90a8
Tanya Makarova <tanyamakarova121290@gmail.com>
2026-04-10 14:29:54 +0300

    Refactoring the calculator: the Calculate function has been removed, errors have been fixed

--- calculator/calculator.h
+++ calculator/calculator.h
@@ -7,6 +7,26 @@ using Number = double;
 // и сохранить его в Number.
 bool ReadNumber(Number& entered_digit);
 
+enum class Operation {
+    Memory,     // s
+    Load,       // l
+    Print,      // =
+    Clear,      // c
+    Quit,       // q
+    Addition,   // +
+    Subtract,   // -
+    Mul,        // *
+    Div,        // /
+    Pow,        // **
+    Root,       // root
+    Assign,     // :
+    Unknown
+};
+//Преобразовываем строку операции в конкретный тип
+Operation StringToOperation(const std::string& operation); 
+
+bool Calculate(Number& curr_number, Operation operation, Number operand);
+
 // Возвращает true, если работа завершилась командой q.
 // Если ошибкf - возвращает false.
 bool RunCalculatorCycle();
